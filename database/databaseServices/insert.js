974d9bfc21d08c60779f9844b01066223b742c0f
Juan Pablo Guevara López <juanpabloguevara8b@gmail.com>
2024-08-14 19:23:23 -0500

    Updated convert querys to promise

--- database/databaseServices/insert.js
+++ database/databaseServices/insert.js
@@ -1,26 +1,44 @@
-import { db, closeDatabase } from '../database';
+import { db } from '../database'
 
 // Insertar un usuario
 const insertUser = (username, email, password) => {
-  const sql = `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`;
-  db.run(sql, [username, email, password], function (err) {
-    if (err) {
-      console.log(username, email, password)
-      return console.error('Error al insertar usuario:', err.message);
-    }
-    console.log(`Usuario añadido con ID: ${this.lastID}`);
-  });
-};
+  return new Promise((resolve, reject) => {
+    const sql = `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`
+    db.run(sql, [username, email, password], function (err) {
+      if (err) {
+        console.error('Error al insertar usuario:', err.message)
+        return reject(err)
+      }
+      console.log(`Usuario añadido con ID: ${this.lastID}`)
+      resolve(this.lastID)
+    })
+  })
+}
 
 // Insertar un evento
-const insertEvent = (user_id, title, description, start_time, end_time, location) => {
-  const sql = `INSERT INTO events (user_id, title, description, start_time, end_time, location) VALUES (?, ?, ?, ?, ?, ?)`;
-  db.run(sql, [user_id, title, description, start_time, end_time, location], function (err) {
-    if (err) {
-      return console.error('Error al insertar evento:', err.message);
-    }
-    console.log(`Evento añadido con ID: ${this.lastID}`);
-  });
-};
+const insertEvent = (
+  user_id,
+  title,
+  description,
+  start_time,
+  end_time,
+  location,
+) => {
+  return new Promise((resolve, reject) => {
+    const sql = `INSERT INTO events (user_id, title, description, start_time, end_time, location) VALUES (?, ?, ?, ?, ?, ?)`
+    db.run(
+      sql,
+      [user_id, title, description, start_time, end_time, location],
+      function (err) {
+        if (err) {
+          console.error('Error al insertar evento:', err.message)
+          return reject(err)
+        }
+        console.log(`Evento añadido con ID: ${this.lastID}`)
+        resolve(this.lastID)
+      },
+    )
+  })
+}
 
-export default { insertUser, insertEvent };
+export { insertUser, insertEvent }
